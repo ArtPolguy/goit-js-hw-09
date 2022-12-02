31b64b231acecba4efcc42d05d0f85ec93b7d132
ArtPolguy <107943545+ArtPolguy@users.noreply.github.com>
2022-12-02 17:42:55 +0000

    Deploying to gh-pages from @ ArtPolguy/goit-js-hw-09@b862185f99e028e28f3d7336aaca76f2e3901565 🚀

---
+++ 01-color-switcher.187a94e3.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.body};let n=null;function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(o){t.stopBtn.disabled=!1,t.startBtn.disabled=!0,n=setInterval(e,1e3)})),t.stopBtn.addEventListener("click",(function(e){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.187a94e3.js.map
